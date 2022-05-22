0c2b1f8c1c393b4878f0437d11f017dd848e4482
erikstiven <105946950+erikstiven@users.noreply.github.com>
2022-05-22 00:01:15 -0500

    Agregar archivos de proyecto.

---
+++ WinAppMatriz/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace WinAppMatriz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblPar = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblSumElemen = new System.Windows.Forms.Label();
+            this.lblNegativos = new System.Windows.Forms.Label();
+            this.lblPositivo = new System.Windows.Forms.Label();
+            this.TxtColumnas = new System.Windows.Forms.TextBox();
+            this.txtFilas = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label6.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(468, 516);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(243, 35);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "suma elementos par";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(468, 441);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(347, 35);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "suma de todos los elementos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(468, 374);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(221, 35);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "contador negativo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(468, 317);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 35);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Contador positvo";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.Lime;
+            this.btnCalcular.Location = new System.Drawing.Point(187, 400);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(150, 51);
+            this.btnCalcular.TabIndex = 23;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblPar
+            // 
+            this.lblPar.AutoSize = true;
+            this.lblPar.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPar.Location = new System.Drawing.Point(965, 535);
+            this.lblPar.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblPar.Name = "lblPar";
+            this.lblPar.Size = new System.Drawing.Size(28, 35);
+            this.lblPar.TabIndex = 22;
+            this.lblPar.Text = "*";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(65, 250);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 23);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "columnas:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(80, 311);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 23);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "filas:";
+            // 
+            // lblSumElemen
+            // 
+            this.lblSumElemen.AutoSize = true;
+            this.lblSumElemen.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSumElemen.Location = new System.Drawing.Point(965, 449);
+            this.lblSumElemen.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblSumElemen.Name = "lblSumElemen";
+            this.lblSumElemen.Size = new System.Drawing.Size(28, 35);
+            this.lblSumElemen.TabIndex = 19;
+            this.lblSumElemen.Text = "*";
+            // 
+            // lblNegativos
+            // 
+            this.lblNegativos.AutoSize = true;
+            this.lblNegativos.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNegativos.Location = new System.Drawing.Point(965, 382);
+            this.lblNegativos.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblNegativos.Name = "lblNegativos";
+            this.lblNegativos.Size = new System.Drawing.Size(28, 35);
+            this.lblNegativos.TabIndex = 18;
+            this.lblNegativos.Text = "*";
+            // 
+            // lblPositivo
+            // 
+            this.lblPositivo.AutoSize = true;
+            this.lblPositivo.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPositivo.Location = new System.Drawing.Point(965, 317);
+            this.lblPositivo.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblPositivo.Name = "lblPositivo";
+            this.lblPositivo.Size = new System.Drawing.Size(28, 35);
+            this.lblPositivo.TabIndex = 17;
+            this.lblPositivo.Text = "*";
+            // 
+            // TxtColumnas
+            // 
+            this.TxtColumnas.Location = new System.Drawing.Point(187, 244);
+            this.TxtColumnas.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.TxtColumnas.Name = "TxtColumnas";
+            this.TxtColumnas.Size = new System.Drawing.Size(196, 30);
+            this.TxtColumnas.TabIndex = 16;
+            this.TxtColumnas.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtColumnas_KeyPress);
+            // 
+            // txtFilas
+            // 
+            this.txtFilas.Location = new System.Drawing.Point(187, 311);
+            this.txtFilas.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.txtFilas.Name = "txtFilas";
+            this.txtFilas.Size = new System.Drawing.Size(196, 30);
+            this.txtFilas.TabIndex = 15;
+            this.txtFilas.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtFilas_KeyPress);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(635, 16);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(6, 7, 6, 7);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(402, 269);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.txtFilas);
+            this.panel1.Controls.Add(this.TxtColumnas);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.btnCalcular);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(459, 1011);
+            this.panel1.TabIndex = 28;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Comic Sans MS", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(34, 40);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(298, 38);
+            this.label8.TabIndex = 25;
+            this.label8.Text = "EJERCICIO MATRIZ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(104, 160);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(240, 46);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "Ingrese  el número de Filas y\r\n Columnas de la matriz";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.lblPositivo);
+            this.panel2.Controls.Add(this.lblNegativos);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.lblSumElemen);
+            this.panel2.Controls.Add(this.lblPar);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1305, 1011);
+            this.panel2.TabIndex = 29;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1305, 1011);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblPar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblSumElemen;
+        private System.Windows.Forms.Label lblNegativos;
+        private System.Windows.Forms.Label lblPositivo;
+        private System.Windows.Forms.TextBox TxtColumnas;
+        private System.Windows.Forms.TextBox txtFilas;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ WinAppMatriz/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinAppMatriz
+{
+    public partial class Form1 : Form
+    {
+        Random objRnd = new Random();//para generar numeros aleatorios
+        int po, ne, sum, sumpar;//variables de resultados
+
+        private void TxtColumnas_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar) == (char)Keys.Enter)
+            {
+                try
+                {
+
+
+                    if (int.Parse(TxtColumnas.Text) >= 1 && int.Parse(TxtColumnas.Text) <= 5)
+                    {
+                        txtFilas.Focus();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Debe ingresar valores de 1-5", "AVISO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("No puede dejar este campo vacio", "AVISO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void txtFilas_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ((e.KeyChar) == (char)Keys.Enter)
+            {
+                try
+                {
+
+
+                    if (int.Parse(TxtColumnas.Text) >= 1 && int.Parse(TxtColumnas.Text) <= 5)
+                    {
+
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Debe ingresar valores de 1-5", "AVISO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("No puede dejar este campo vacio", "AVISO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int colum = int.Parse(TxtColumnas.Text);
+            int filas = int.Parse(txtFilas.Text);
+
+            int[,] vec = new int[10, 10];//declaro matriz
+            //HAGO LOS CALCULOS
+            for (int k = 0; k < filas; k++)
+            {
+                for (int i = 0; i < colum; i++)
+                {
+                    vec[k, i] = objRnd.Next(-9, 9);
+                    if (vec[k, i] >= 1)
+                    {
+                        po++;//sumo positivos
+                    }
+                    else if (vec[k, i] < 0)
+                    {
+                        ne++;//sumo negativos
+                    }
+                    sum += vec[k, i];//sumo todos los elementos
+                    if (i % 2 != 0)
+                    {
+                        sumpar += vec[k, i];//sumo los elementos de las columnas pares
+                    }
+                }
+            }
+            //DIMENSIONO EL DATAGRINDVIEW
+            dataGridView1.RowCount = filas;
+            dataGridView1.ColumnCount = colum;
+            //GUARDO LOS DATOS DE LA MATRIZ EN UNA DATAGRINDVIEW
+            for (int x = 0; x < filas; x++)
+            {
+                for (int j = 0; j < colum; j++)
+                {
+                    dataGridView1.Rows[x].Cells[j].Value = vec[x, j];
+                }
+            }
+
+            lblPositivo.Text = po.ToString();
+            lblNegativos.Text = ne.ToString();
+            lblSumElemen.Text = sum.ToString();
+            lblPar.Text = sumpar.ToString();
+
+        }
+
+
+    }
+}
